7bc41097727afa76debbf72f3ecbc0bf8a188333
Tony Butler <tonyjbutler@gmail.com>
2015-05-22 14:32:06 +0100

    mod_questionnaire: Change type of questionnaire_response.username to int

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version  = 2014070403;  // The current module version (Date: YYYYMMDDXX)
+$plugin->version  = 2014070404;  // The current module version (Date: YYYYMMDDXX)
 $plugin->requires = 2014050800; // Moodle version.
 
 $plugin->component = 'mod_questionnaire';
